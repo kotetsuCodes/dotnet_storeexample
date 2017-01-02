5a636ee016167dce40f9a0717a94a27cdba84c39
Carl Brown <carllbrown87@gmail.com>
2017-01-01 23:44:40 -0600

    Added remove from cart buttons and checkout page.

---
+++ storeexample/Controllers/CheckoutController.cs
@@ -0,0 +1,31 @@
+﻿using storeexample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace storeexample.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private StoreExampleModel db = new StoreExampleModel();
+
+        // GET: Checkout
+        public ActionResult Index(int orderId)
+        {
+            var order = db.Orders.SingleOrDefault(o => o.OrderId == orderId);
+            if (order == null)
+            {
+                throw new Exception("Invalid Order");
+            }
+
+            var model = new CheckoutViewModel()
+            {
+                Order = order
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ storeexample/Models/CheckoutViewModels.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace storeexample.Models
+{
+    public class CheckoutViewModel
+    {
+        public Order Order { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string DeliveryAddress { get; set; }
+        [Required]
+        public int ZipCode { get; set; }
+        [Required]
+        public string CityState { get; set; }
+        [Display(Name = "Special Instructions")]
+        public string SpecialInstructions { get; set; }
+        [Display(Name = "Delivery Day")]
+        [Required]
+        public string DeliveryDay { get; set; }
+        [Display(Name = "Delivery Time")]
+        [Required]
+        public string DeliveryTime { get; set; }
+    }
+}
