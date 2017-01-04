6f00e25f70bf063ba537a77b81ae7dd1fb734fbe
Carl Brown <carllbrown87@gmail.com>
2017-01-03 22:35:53 -0600

    Removed uneeded commented code.

--- storeexample/App_Start/BundleConfig.cs
+++ storeexample/App_Start/BundleConfig.cs
@@ -13,9 +13,6 @@ namespace storeexample
                         "~/Scripts/jquery.unobtrusive-ajax*",
                         "~/Scripts/jquery.validate*"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-            //            "~/Scripts/jquery.validate*"));
-
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
